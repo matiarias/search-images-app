dbd5e8d5a2f4c0dd294cf0140370590d2b57bc76
Matias Arias <matt.arias182@gmail.com>
2022-09-25 21:09:32 -0300

    se instalo todas las dependencias para comenzar

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl text-green-800 bg-yellow-600">Search Images</h1>
+    </div>
+  );
+}
+
+export default App;
