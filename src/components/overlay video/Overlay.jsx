a47edddb567db74005469edbf8d7e9d34342594b
Matias Arias <matt.arias182@gmail.com>
2022-10-03 19:04:52 -0300

    agregue el fetch para consumir la api, el input search y react toastify

--- src/components/overlay video/Overlay.jsx
+++ src/components/overlay video/Overlay.jsx
@@ -1,7 +1,37 @@
 import React from "react";
 import videoBg from "../../assets/ocean-bg.mp4";
+import { BsSearch } from "react-icons/bs";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-const Overlay = () => {
+const Overlay = ({ inputValue, setInputValue, term, setTerm }) => {
+  const handleChange = ({ target }) => {
+    setInputValue(target.value);
+    // console.log(target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (inputValue.trim() !== "") {
+      setTerm(inputValue.trim());
+      // console.log(term);
+      setInputValue("");
+    } else {
+      // console.log("ingresa algo");
+      toast.warn(" Por favor ingresa algo de tu interes", {
+        theme: "light",
+        position: "top-center",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    }
+  };
+
   return (
     <div className="relative h-screen w-full">
       <video
@@ -12,15 +42,62 @@ const Overlay = () => {
         muted
       ></video>
 
-      <div className="absolute top-0 left-0 h-full w-full bg-gray-900/30"></div>
+      <div className="absolute top-0 left-0 h-full w-full bg-gray-900/40"></div>
       <div className="absolute top-0 left-0 h-full w-full px-8 flex flex-col justify-center items-center gap-4">
-        <h1 className="text-white text-2xl sm:text-3xl md:text-5xl font-bold">
+        <h1 className="text-white text-2xl sm:text-3xl md:text-5xl font-bold text-center">
           Increíbles Imágenes Gratis Para Descargar
         </h1>
-        <p className="text-white text-sm sm:text-base md:text-lg">
+        <h2 className="text-white text-sm sm:text-base md:text-lg text-center">
           Nuestro banco de imágenes tiene más de 1 millón de imágenes y videos
           compartidos por nuestra talentosa comunidad.
-        </p>
+        </h2>
+
+        <form
+          onSubmit={handleSubmit}
+          className="w-full sm:w-[600px] md:w-[700px] p-4 flex justify-between items-center bg-gray-300 rounded-3xl mt-4"
+        >
+          <input
+            className="w-full bg-transparent focus:outline-none placeholder:text-gray-700"
+            type="text"
+            placeholder="Buscar imagenes"
+            autoFocus
+            name={term}
+            value={inputValue}
+            onChange={handleChange}
+          />
+
+          <button className="px-4 py-1" type="submit">
+            <BsSearch className="text-xl text-gray-900 hover:text-green-700" />
+          </button>
+        </form>
+
+        <div className="mt-2 flex gap-2 flex-wrap">
+          <span className="text-white font-medium text-sm hover:underline cursor-pointer">
+            flores
+          </span>
+          <span className="text-white font-medium text-sm hover:underline-offset-2 hover:underline cursor-pointer">
+            paisajes
+          </span>
+          <span className="text-white font-medium text-sm hover:underline-offset-2 hover:underline cursor-pointer">
+            cielo
+          </span>
+          <span className="text-white font-medium text-sm hover:underline-offset-2 hover:underline cursor-pointer">
+            playa
+          </span>
+          <span className="text-white font-medium text-sm hover:underline-offset-2 hover:underline cursor-pointer">
+            musica
+          </span>
+          <span className="text-white font-medium text-sm hover:underline-offset-2 hover:underline cursor-pointer">
+            naturaleza
+          </span>
+          <span className="text-white font-medium text-sm hover:underline-offset-2 hover:underline cursor-pointer">
+            paris
+          </span>
+          <span className="text-white font-medium text-sm hover:underline-offset-2 hover:underline cursor-pointer">
+            personas
+          </span>
+        </div>
+        <ToastContainer />
       </div>
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Overlay from "../components/overlay video/Overlay";
 
 const Home = () => {
+  const [data, setData] = useState([]);
+
+  const [inputValue, setInputValue] = useState("");
+
+  const [term, setTerm] = useState("");
+
+  const [loading, setLoading] = useState(true);
+
+  const pixabayApiCall = async (query) => {
+    try {
+      const resp = await fetch(
+        `https://pixabay.com/api/?q=${query}&key=28962423-2061919b5fb3ab8799e9f4b1a`
+      );
+
+      const results = await resp.json();
+      console.log(results);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    pixabayApiCall(term);
+  }, [term]);
+
   return (
     <div>
-      <Overlay />
+      <Overlay
+        inputValue={inputValue}
+        setInputValue={setInputValue}
+        term={term}
+        setTerm={setTerm}
+      />
     </div>
   );
 };
