f3f0348414edd269d3361cea04d47fab457d09ef
Matias Arias <matt.arias182@gmail.com>
2022-10-05 23:43:47 -0300

    se agrego react infinite scroll

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Loading from "../components/loading/Loading";
 import Overlay from "../components/overlay video/Overlay";
+// import InfiniteScroll from "react-infinite-scroll-component";
 
 const Home = () => {
   const [data, setData] = useState({});
@@ -11,15 +12,21 @@ const Home = () => {
 
   const [loading, setLoading] = useState(true);
 
+  const [page, setPage] = useState(1);
+
+  const [hasMore, setHasMore] = useState(true);
+
+  const perPage = 20;
+
   const pixabayApiCall = async (query) => {
     try {
       const resp = await fetch(
-        `https://pixabay.com/api/?q=${query}&key=28962423-2061919b5fb3ab8799e9f4b1a&page=1&per_page=20`
+        `https://pixabay.com/api/?q=${query}&key=28962423-2061919b5fb3ab8799e9f4b1a&page=${1}&per_page=${perPage}`
       );
 
       const results = await resp.json();
-      console.log(results);
-      setData(results);
+      console.log(results.hits);
+      setData(results.hits);
       setLoading(false);
     } catch (error) {
       console.log(error);
@@ -27,17 +34,19 @@ const Home = () => {
   };
 
   useEffect(() => {
-    pixabayApiCall(term);
-  }, [term]);
+    pixabayApiCall(term, perPage);
+  }, [term, perPage]);
+
+  const fetchData = async () => {};
 
   return (
     <div>
-      {/* <Overlay
+      <Overlay
         inputValue={inputValue}
         setInputValue={setInputValue}
         term={term}
         setTerm={setTerm}
-      /> */}
+      />
 
       {loading ? (
         <div className="w-full text-center mt-12">
@@ -45,14 +54,14 @@ const Home = () => {
         </div>
       ) : (
         <div
-          className="columns-1 sm:columns-2 md:columns-3 space-y-4 gap-6 md:gap-4 py-12 px-8 
-      bg-gray-200"
+          className="columns-1 sm:columns-2 md:columns-3 space-y-4 gap-x-4 py-12 px-8
+        bg-gray-200"
         >
-          {data.hits
-            ? data.hits.map((image) => (
+          {data
+            ? data.map((image) => (
                 <div
                   key={image.id}
-                  className="relative h-auto w-full lg:max-w-[500px]"
+                  className="relative h-auto w-full xl:max-w-[500px] border-2 border-gray-400"
                 >
                   <div className="absolute top-0 left-0 h-full w-full hover:bg-gray-700/20"></div>
                   <img
